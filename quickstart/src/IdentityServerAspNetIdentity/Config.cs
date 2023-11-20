92282baae8881192ffdf9546ba9dca4f430b8f65
Buwaneka Saranga <bsaranga@gmail.com>
2023-11-20 23:05:37 +0530

    added new aspnetidentity project and configured it

--- quickstart/src/IdentityServerAspNetIdentity/Config.cs
+++ quickstart/src/IdentityServerAspNetIdentity/Config.cs
@@ -1,52 +1,70 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using IdentityModel;
 
 namespace IdentityServerAspNetIdentity;
 
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        {
+        new[]
+        { 
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
+            new IdentityResource
+            {
+                Name = "verification",
+                UserClaims = new List<string>
+                {
+                    JwtClaimTypes.Email,
+                    JwtClaimTypes.EmailVerified
+                }
+            },
+            new IdentityResource
+            {
+                Name = "designation",
+                UserClaims = new List<string>
+                {
+                    "designation"
+                }
+            },
+            new IdentityResource("color", new[] { "favorite_color" })
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
-        {
-            new ApiScope("scope1"),
-            new ApiScope("scope2"),
-        };
+        new[]
+            { new ApiScope("api1", "MyAPI") };
 
     public static IEnumerable<Client> Clients =>
-        new Client[]
+        new[]
         {
-            // m2m client credentials flow client
             new Client
             {
-                ClientId = "m2m.client",
-                ClientName = "Client Credentials Client",
-
+                ClientId = "client",
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                AllowedScopes = { "scope1" }
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = { "api1" }
             },
-
-            // interactive client using code flow + pkce
             new Client
             {
-                ClientId = "interactive",
-                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256()) },
                 AllowedGrantTypes = GrantTypes.Code,
-
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
+                RedirectUris = { "https://localhost:5002/signin-oidc" },
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
                 AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
-            },
+                AllowedScopes = new List<string>()
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "verification",
+                    "designation",
+                    "api1",
+                    "color"
+                }
+            }
         };
 }
--- quickstart/src/WebClient/Program.cs
+++ quickstart/src/WebClient/Program.cs
@@ -24,12 +24,14 @@ builder.Services.AddAuthentication(options =>
         options.Scope.Add("profile");
         options.Scope.Add("verification");
         options.Scope.Add("designation");
+        options.Scope.Add("color");
         
         options.Scope.Add("api1");
         options.Scope.Add("offline_access");
         
         options.ClaimActions.MapJsonKey("email_verified", "email_verified");
         options.ClaimActions.MapJsonKey("designation", "designation");
+        options.ClaimActions.MapUniqueJsonKey("favorite_color", "favorite_color");
         options.GetClaimsFromUserInfoEndpoint = true; // adds the user claims to the token
         
         options.SaveTokens = true;
