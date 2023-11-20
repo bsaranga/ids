81a582fa1b8a7ab74a7c6705df3750b954167d0b
Buwaneka Saranga <bsaranga@gmail.com>
2023-11-20 19:31:39 +0530

    added the api1 scope so that the web client can request it on user's behalf

--- quickstart/src/WebClient/Pages/CallApi.cshtml.cs
+++ quickstart/src/WebClient/Pages/CallApi.cshtml.cs
@@ -1,12 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyApp.Namespace
+namespace WebClient.Pages
 {
     public class CallApiModel : PageModel
     {
-        public void OnGet()
+        public string Json = string.Empty;
+        
+        public async Task OnGet()
         {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = await client.GetStringAsync("https://localhost:6001/api/Identity");
+
+            var parsed = JsonDocument.Parse(content);
+            var formatted = JsonSerializer.Serialize(parsed, new JsonSerializerOptions { WriteIndented = true });
+            
+            Json = formatted;
         }
     }
 }
--- quickstart/src/WebClient/Program.cs
+++ quickstart/src/WebClient/Program.cs
@@ -24,10 +24,14 @@ builder.Services.AddAuthentication(options =>
         options.Scope.Add("profile");
         options.Scope.Add("verification");
         options.Scope.Add("designation");
+        
+        options.Scope.Add("api1");
+        options.Scope.Add("offline_access");
+        
         options.ClaimActions.MapJsonKey("email_verified", "email_verified");
         options.ClaimActions.MapJsonKey("designation", "designation");
         options.GetClaimsFromUserInfoEndpoint = true; // adds the user claims to the token
-
+        
         options.SaveTokens = true;
     });
 
