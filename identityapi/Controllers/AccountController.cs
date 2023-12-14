592583d93ed1ae3390bbee724024641a5ce57473
bsaranga <bsaranga@gmail.com>
2023-12-14 07:33:42 +0530

    new account controller for logging out

---
+++ identityapi/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace identityapi.Controllers
+{
+    [Route("id")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet("logout")]
+        public async Task<IActionResult> LogOut()
+        {
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                await signInManager.SignOutAsync();
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
